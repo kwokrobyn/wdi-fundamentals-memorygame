47362c45cdd624ddfdea2fc5c25aaf423055b051
Robyn Kwok <robyn.kwok@duke.edu>
2017-05-15 14:58:25 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,6 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   }
@@ -13,5 +8,10 @@ if (cardsInPlay.length === 2) {
     alert("Sorry, try again.");
   }
 }
-
-console.log("User flipped " + cardsInPlay[1]);
+flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+flipCard(0);
+flipCard(2);
